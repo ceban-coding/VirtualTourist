81b6633d890721c26e330db22bc15d04befb42b1
Ion Ceban <ceban.ion1992@gmail.com>
2021-06-07 17:19:29 -0500

    added image view and map view to photoAlbumViewController, added button to navigtaionBar

---
+++ VirtualTourist/Controllers/PhotoAlbumViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Ion Ceban on 6/7/21.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+
+    //MARK: - Action Buttons
+    
+    @IBAction func backButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func newCollectionButton(_ sender: Any) {
+        
+    }
+    
+}
--- VirtualTourist/Controllers/ViewController.swift
+++ VirtualTourist/Controllers/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class VirtualTouristViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //MARK: - Action Buttons
+    
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func editButton(_ sender: Any) {
+        
+    }
+    
 }
 
