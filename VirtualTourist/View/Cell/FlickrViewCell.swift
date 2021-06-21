615274f986fe7b092e34e613f3efe79bad9072fd
Ion Ceban <ceban.ion1992@gmail.com>
2021-06-21 09:47:58 -0500

    Created FlickrViewCell file and added photoImage outlet

---
+++ VirtualTourist/View/Cell/FlickrViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FlickrViewCell.swift
+//  VirtualTourist
+//
+//  Created by Ion Ceban on 6/21/21.
+//
+
+import UIKit
+
+class FlickrViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var photoImage: UIImageView!
+    
+}
